1c79cf5c02ccbde94fb4efe355d9b143919caa70
Matt McClellan <mcclellan.mj@gmail.com>
2016-08-02 23:26:55 -0500

    Working on the UI to make use of the icons and improve look

--- src/rendering/files.rs
+++ src/rendering/files.rs
@@ -6,10 +6,13 @@ const MAIN_CSS: &'static str = include_str!("../../resources/main.css");
 
 fn render_file(entry: &DirEntry) -> Box<RenderBox> {
     let file_name = entry.file_name();
+    let file_type = entry.file_type().unwrap();
+
+    let offset = if file_type.is_dir() {"icon-folder"} else {"icon-file"};
     box_html! {
-        li {
-            span(class="file-list-img") : raw!("");
-            a(href="#") : file_name.to_str().unwrap()
+        a(class="file-entry", href="#") {
+            span(class=format!("entry-icon {}", offset)) : raw!("");
+            span : file_name.to_str().unwrap()
         }
     }
 }
@@ -22,14 +25,14 @@ pub fn render<I: Iterator<Item=DirEntry>>(title: &str, files: I) -> String {
         : raw!("<!DOCTYPE html>");
         html {
             head {
-                style(type="text/css") : MAIN_CSS;
+                style(type="text/css") : raw!(MAIN_CSS);
                 title : title
             }
             body {
                 main {
                     header { h1 : title}
                     section(id="files") {
-                        ul(class="file-list") {
+                        div(class="file-list") {
                             @ for file in sorted_files {
                                 : render_file(&file);
                             }
