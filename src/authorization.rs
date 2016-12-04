1229baa7484edcb18dff8aa6ffcb24baf11f4a32
Matt McClellan <mcclellan.mj@gmail.com>
2016-12-03 21:52:42 -0600

    Added encoded original request url in the query for the redirect

--- src/authorization.rs
+++ src/authorization.rs
@@ -4,8 +4,8 @@ use iron::IronError;
 use iron::modifiers::{RedirectRaw};
 use std::io::{Error, ErrorKind};
 use iron::status;
-// use iron::request::Url;
 use iron_sessionstorage::SessionRequestExt;
+use url::percent_encoding;
 use Login;
 
 pub struct AuthorizationMiddleware {
@@ -20,9 +20,13 @@ impl AuthorizationMiddleware {
 impl BeforeMiddleware for AuthorizationMiddleware {
     fn before(&self, req: &mut Request) -> IronResult<()> {
         let result = try!(req.session().get::<Login>()).is_some();
+        let generic_url = req.url.clone().into_generic_url();
+        let mut query = percent_encoding::utf8_percent_encode(
+            generic_url.as_str(),
+            percent_encoding::QUERY_ENCODE_SET);
 
         if !result {
-            let response = (status::Found, RedirectRaw(String::from("/login.html")));
+            let response = (status::Found, RedirectRaw(String::from(format!("/login.html?url={}", query))));
             Err(IronError::new(Error::new(ErrorKind::Other, "Not Authenticated"), response))
         } else {
             Ok(())
