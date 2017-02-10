29b2914a27937466a1f281a40daba018acc60640
Matt McClellan <mcclellan.mj@gmail.com>
2017-02-09 20:25:13 -0600

    Added the form for the multiple file upload

--- web/src/AttributesExtended.elm
+++ web/src/AttributesExtended.elm
@@ -1,7 +1,9 @@
 module AttributesExtended exposing (voidHref)
 import Html exposing (Attribute)
 import Html.Attributes exposing (href)
+import Html.Events
 
 voidHref: Attribute a
 voidHref = href "javascript:;"
+
 
--- web/src/Views/Upload.elm
+++ web/src/Views/Upload.elm
@@ -1,29 +1,61 @@
 module Views.Upload exposing (load, update, Msg, Model, render)
 
 import Files
-import Html exposing (Html, div)
-import Debug
+import Html exposing (Html, div, h1, text, input)
+import Html.Attributes exposing (type_, multiple)
+import Html.Events
 import UI.Components as Components
 import AddressableStates
-import FileReader exposing (NativeFile)
+import FileReader exposing (NativeFile, parseSelectedFiles)
+import Json.Decode as Json exposing (Value, andThen)
 
 type Msg
-  = DoUpload
+  = UploadFiles (List NativeFile)
 
 type alias Model =
   { targetDir: Files.FilePath
+  , selectedFiles: List NativeFile
   }
 
 load : Files.FilePath -> (Model, Cmd Msg)
-load path = ( { targetDir = path }, Cmd.none )
+load path = ( { targetDir = path, selectedFiles = [] }, Cmd.none )
 
 update : Model -> Msg -> (Model, Cmd Msg)
 update model msg =
   case msg of
-    DoUpload -> Debug.crash "DoUpload is not yet implemented"
+    UploadFiles x -> ( { model | selectedFiles = x }, Cmd.none )
+
+onchange : (List NativeFile -> value) -> Html.Attribute value
+onchange action = Html.Events.on "change" (Json.map action parseSelectedFiles)
+
+renderFile : NativeFile -> Html Msg
+renderFile file =
+  div [] [text file.name]
+
+renderFileStatus : Model -> Html Msg
+renderFileStatus model =
+  div [] <| List.map renderFile model.selectedFiles
+
+renderUploadForm : Model -> Html Msg
+renderUploadForm model =
+  div []
+     [ h1 [] [ text "Multiple files with automatic upload" ]
+     , input
+        [ type_ "file"
+        , onchange UploadFiles
+        , multiple True
+        ]
+        []
+     ]
 
 render : Model -> Html Msg
 render model =
-  div
-    []
-    [ Components.closeableHeader "Upload File" <| AddressableStates.generateFolderAddress model.targetDir ]
+  let
+    content = case model.selectedFiles of
+      [] -> renderUploadForm model
+      _ -> renderFileStatus model
+  in
+    div []
+      [ Components.closeableHeader "Upload File" <| AddressableStates.generateFolderAddress model.targetDir
+      , content
+      ]
