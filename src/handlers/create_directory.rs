ac9314db2c77d571dc19f5d2c02c1df64eb799bd
Matt McClellan <mcclellan.mj@gmail.com>
2017-04-05 22:45:18 -0500

    Finished up code for creating a directory

--- src/handlers/create_directory.rs
+++ src/handlers/create_directory.rs
@@ -10,6 +10,7 @@ use iron::headers::ContentType;
 
 use std::path::{Path, PathBuf};
 use std::fs::copy;
+use std::fs::create_dir;
 use std::io::Read;
 
 use params::Params as IronParams;
@@ -42,8 +43,20 @@ impl Handler for CreateDirectoryHandler {
         req.body.read_to_string(&mut request_body).unwrap();
         let create_directory_request: CreateDirectoryRequest = itry!(json::decode(&request_body));
 
-        println!("{:?}", create_directory_request);
+        let mut request_path = PathBuf::from(create_directory_request.base_path);
+        request_path.push(create_directory_request.new_directory);
 
-        Ok(Response::with((status::NotImplemented)))
+        let is_child = itry!(filetools::is_child_of_safe(&self.root_folder, &request_path.parent().unwrap().to_path_buf()));
+
+        if !is_child {
+            Ok(Response::with((status::BadRequest, "Trying to create directory outside of root")))
+        } else {
+            if request_path.as_path().exists() {
+                Ok(Response::with((status::BadRequest, "Folder already exists")))
+            } else {
+                itry!(create_dir(request_path));
+                Ok(Response::with((status::Ok, "null")))
+            }
+        }
     }
 }
